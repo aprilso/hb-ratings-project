114efcc5f00375eb9e38dd850ea245244b4173e9
vagrant <vagrant@vagrant.vm>
2021-05-28 03:58:50 +0000

    Finished Part 2!

--- seed_database.py
+++ seed_database.py
@@ -10,7 +10,32 @@ import model
 import server
 
 os.system('dropdb ratings')
-# More code will go here
+os.system('createdb ratings')
 
-##START HERE: https://fellowship.hackbrightacademy.com/materials/pt9g/exercises/ratings-v2/index-2.html 
-# "Seed the Database"
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+  movie_data = json.loads(f.read())
+#question - when/how to use with?
+
+movies_in_db = []
+for movie in movie_data:
+    title, overview, poster_path = (movie['title'], movie['overview'], movie['poster_path'])
+    release_date = datetime.strptime(movie['release_date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+
+    movies_in_db.append(db_movie)
+
+for n in range(10):
+    email = f'user{n}@test.com'
+    password = "fakepassword"
+
+    db_user = crud.create_user(email, password)
+
+    for i in range(10):
+        movie = choice(movies_in_db)
+        rating = randint(1,5)
+
+        db_ratings = crud.create_rating(db_user, movie, rating) 
